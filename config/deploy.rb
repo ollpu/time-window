2cbad25f44be9b3fbede9a083863b569a979fdee
Roope Salmi <rpsalmi@gmail.com>
2016-03-01 09:27:23 +0200

    Update username

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,8 +6,8 @@ server '37.139.14.235', port: 22, roles: [:web, :app, :db], primary: true
 set :application, 'time-window'
 set :repo_url, 'git@github.com:ollpu/time-window.git'
 
-set :application,     'appname'
-set :user,            'deploy'
+set :application,     'time-window'
+set :user,            'ollpu'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
