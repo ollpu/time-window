75998c683019319b4b36966a6fbe7f9d4805dc1c
Roope Salmi <rpsalmi@gmail.com>
2016-03-15 09:07:47 +0200

    Add allowed Origins for cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.action_cable.allowed_request_origins = ["http://37.139.14.235", "ws://37.139.14.235", "https://37.139.14.235"]
 end
